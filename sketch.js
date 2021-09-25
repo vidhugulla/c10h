590588c366915de15019437d3aa4267d1fe675b9
vidhugulla <90997172+vidhugulla@users.noreply.github.com>
2021-09-25 18:55:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var sea =CreateSprite();
+sea.setAnimation("sea.png");
+function preload(){
+seaImg = loadImage("sea.png");
+shipImg1 = loadAnimation("ship-1.png","ship-2.png") 
+}
+
+function setup(){
+  createCanvas(400,400);
+  sea.addImage("ship-3.png")
+}
+
+function draw() {
+  background("blue");
+ if(sea.x < 0){
+   sea.x - sea.width/2;
+ }
+ drawSprites();
+}
